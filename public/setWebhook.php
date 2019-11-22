0bf182e383bb57e13590f9aea8dde2a0d2edf270
fomotoshi <fomotoshi@ya.ru>
2019-11-22 12:29:40 +0300

    created webhook registration for telegram

---
+++ public/setWebhook.php
@@ -0,0 +1,14 @@
+<?php
+
+const TOKEN = '784355426:AAEgw0aJ01hx3PAr3AtAQSff6FF-c6T_bfw';
+$method = 'setWebhook';
+
+
+$url = 'https://api.telegram.org/bot' . TOKEN . '/' . $method;
+$options = [
+    'url' => 'https://fomotoshi.dev',
+];
+
+$response = file_get_contents($url . '?' . http_build_query($options));
+
+var_dump($response);
